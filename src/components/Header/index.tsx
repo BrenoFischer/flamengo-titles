8cac67b55b5e0f3ae927c4c06b4848327ff23e48
Breno Fischer <breno_perricone@hotmail.com>
2023-08-08 16:03:17 -0300

    finish header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
-import { HeaderContainer, UpperHeader } from './styles'
+import { UpperHeader } from './styles'
 import FlamengoLogo from '../../assets/flamengo.png'
 
 export default function Header() {
   return (
-    <HeaderContainer>
+    <header>
       <UpperHeader>
+        <p>
+          Uma vez <span>Flamengo</span>,
+        </p>
         <img src={FlamengoLogo} alt="Flamengo Rowing Shield" />
+        <p>
+          sempre <span>Flamengo</span>.
+        </p>
       </UpperHeader>
       <div></div>
-    </HeaderContainer>
+    </header>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,21 +1,31 @@
 import { styled } from 'styled-components'
 
-export const HeaderContainer = styled.header`
-  /* padding: 2rem 0; */
-`
-
 export const UpperHeader = styled.div`
   background: ${(props) => props.theme.red};
   height: 6rem;
-  padding: 1rem 0;
+  width: 100%;
 
   display: flex;
   justify-content: center;
+  align-items: center;
   box-shadow: 0 0 15px #000;
 
+  position: relative;
+
+  font-family: 'Titillium Web', sans-serif;
+  font-weight: 700;
+  font-size: 1.5rem;
+  color: ${(props) => props.theme.black};
+
+  span {
+    color: ${(props) => props.theme.white};
+  }
+
   img {
+    position: relative;
+    top: 50%;
+    right: 0;
     height: 10rem;
-    /* box-shadow: 0 0 15px #000; */
     -webkit-filter: drop-shadow(0 0 3px #000);
     filter: drop-shadow(0 0 3px #000);
   }
